4af094806d17066ae48cfe794377d45644d34273
Bogdan Bogdanovic <bogdi.mail@gmail.com>
2025-03-20 15:47:51 +0100

    streamline initialization process in app.ts

--- src/app.ts
+++ src/app.ts
@@ -11,28 +11,24 @@ import App from "./views/App";
 import Inputs from "./views/Inputs";
 
 (async function () {
-  const init = async () => {
-    try {
-      // Loading
-      App.renderLoading();
+  try {
+    // Loading
+    App.renderLoading();
 
-      // Get data
-      await model.getDictionary();
+    // Get data
+    await model.getDictionary();
 
-      // Render
-      App.render();
-      Inputs.render();
+    // Render
+    App.render();
+    Inputs.render();
 
-      // Add controls
-      controlForm();
-      controlInputs();
-      reset();
-      generateRandomChars();
-      findSolutions();
-    } catch (error) {
-      App.renderError(error);
-    }
-  };
-
-  await init();
+    // Add controls
+    controlForm();
+    controlInputs();
+    reset();
+    generateRandomChars();
+    findSolutions();
+  } catch (error) {
+    App.renderError(error);
+  }
 })();
